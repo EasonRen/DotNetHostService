91d1c391418739e4faa5a5787e975c8d83c0e083
Eason Ren <eason.ren@outlook.com>
2018-08-07 19:43:08 +0800

    Add project files.

---
+++ DotNetHostService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetHostService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
